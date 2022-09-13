4296f4343cce098c5156886ff126f921b4199eb1
SaschaPetro <petrovits1987@gmail.com>
2022-09-13 17:20:34 +0200

    add author

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ function getQuote() {
   fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
     .then((response) => response.json())
     .then((data) => {
+      console.log(data.data);
       for (let quoteData of data.data) {
         const listItem = document.getElementById("quote");
+        const author = document.getElementById("quote-author");
         listItem.innerHTML = quoteData.quoteText;
+        author.innerHTML = quoteData.quoteAuthor;
         document.querySelector(".list").appendChild(listItem);
+        document.querySelector(".list").appendChild(author);
       }
     });
 }
